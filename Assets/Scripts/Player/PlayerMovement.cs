7d70c73866b217a194467e57ec32dd04402c6c05
RVerhoef <r.verhoef@hotmail.nl>
2015-10-02 11:31:15 +0200

    First Commit

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿//Written by Rob Verhoef
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	private float _speed = 5;
+	private Rigidbody2D _rigidBody;
+	private Vector3 _mousePos;
+
+	void Awake () 
+	{
+		_rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+	void FixedUpdate () 
+	{
+		//Player can move in 8 directions by pressing a combinations of the movement keys
+		if(Input.GetAxis ("Horizontal") < 0 || Input.GetAxis ("Horizontal") > 0)
+		{
+			_rigidBody.velocity = new Vector2 ((Input.GetAxis ("Horizontal") * (_speed)), _rigidBody.velocity.y);
+		}
+		else
+		{
+			_rigidBody.velocity = new Vector2 (0, _rigidBody.velocity.y);
+		}
+
+		if(Input.GetAxis ("Vertical") < 0 || Input.GetAxis ("Vertical") > 0)
+		{
+			_rigidBody.velocity = new Vector2 (_rigidBody.velocity.x, (Input.GetAxis ("Vertical") * (_speed)));
+		}
+		else
+		{
+			_rigidBody.velocity = new Vector2 (_rigidBody.velocity.x, 0);
+		}
+		//Player rotates towards the mouse pointer / recticle 
+		_mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		transform.rotation = Quaternion.LookRotation(Vector3.forward, _mousePos - transform.position);
+    }
+}
---
+++ Assets/Scripts/UI/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿//Written by Rob Verhoef
+using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private Transform _player;
+    private float _followSpeed = 5f;
+
+	void Awake ()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+	
+	void FixedUpdate ()
+    {
+        float step = _followSpeed * Time.smoothDeltaTime;
+
+        //Camera follows player with a slight delay
+        if (_player != null)
+        {
+            transform.position = Vector3.Lerp (transform.position, new Vector3(_player.transform.position.x, _player.transform.position.y, -10), step);
+        }
+    }
+}
