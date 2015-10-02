9844a032e2a40a92f47e6004f8b27044994c8ed4
RVerhoef <r.verhoef@hotmail.nl>
2015-10-02 15:05:45 +0200

    Started work on guard AI / Finite State Machine

---
+++ Assets/Scripts/Enemies/GuardBehaviour.cs
@@ -0,0 +1,39 @@
+﻿//Written by Rob Verhoef
+using UnityEngine;
+using System.Collections;
+
+public class GuardBehaviour : MonoBehaviour
+{
+    public State currentState;
+
+	void Start ()
+    {
+
+	}
+
+	void Update ()
+    {
+	
+	}
+
+    void SetCurrentState(State state)
+    {
+        currentState = state;
+    }
+
+    void SwitchState()
+    {
+        switch (currentState)
+        {
+
+        }
+    }
+
+    public enum State
+    {
+        PATROL,
+        ALERT,
+        ARREST,
+        INVESTIGATE
+    }
+}
