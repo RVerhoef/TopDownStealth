1281cf319d6f52298d36b0a7f7ab78e8ce807d1e
RVerhoef <r.verhoef@hotmail.nl>
2015-10-04 22:40:15 +0200

    Worked on guard AI / Finite State Machine

--- Assets/Scripts/Enemies/GuardBehaviour.cs
+++ Assets/Scripts/Enemies/GuardBehaviour.cs
@@ -6,14 +6,16 @@ public class GuardBehaviour : MonoBehaviour
 {
     public State currentState;
 
+    public GameObject Player;
+
 	void Start ()
     {
+        SetCurrentState(State.PATROL);
+    }
 
-	}
-
-	void Update ()
+	void FixedUpdate ()
     {
-	
+        StateSwitcher();
 	}
 
     void SetCurrentState(State state)
@@ -21,12 +23,49 @@ public class GuardBehaviour : MonoBehaviour
         currentState = state;
     }
 
-    void SwitchState()
+    void StateSwitcher()
     {
         switch (currentState)
         {
-
+            case State.PATROL:
+                Patrol();
+                break;
+            case State.ALERT:
+                Alert();
+                break;
+            case State.ARREST:
+                Arrest();
+                break;
+            case State.INVESTIGATE:
+                Investigate();
+                break;
         }
+    }
+
+    void Patrol()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, 100);
+        Debug.DrawRay(transform.position, Vector2.up, Color.green);
+        if (hit.collider.gameObject.tag == "Player")
+        {
+            Debug.Log("I see you!");
+            SetCurrentState(State.ALERT);
+        }
+    }
+
+    void Alert()
+    {
+
+    }
+
+    void Arrest()
+    {
+
+    }
+
+    void Investigate()
+    {
+
     }
 
     public enum State
