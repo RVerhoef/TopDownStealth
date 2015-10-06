94742f93a8992c620c7fc3f4b5599cab967fd46b
RVerhoef <r.verhoef@hotmail.nl>
2015-10-06 15:09:14 +0200

    Started work on navigation for guard AI

---
+++ Assets/Scripts/UI/NavigationFix.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class NavigationFix : Editor
+{
+    [MenuItem("NavMesh/Build With Slope 90")]
+    public static void BuildSlope90()
+    {
+        SerializedObject obj = new SerializedObject(NavMeshBuilder.navMeshSettingsObject);
+        SerializedProperty prop = obj.FindProperty("m_BuildSettings.agentSlope");
+        prop.floatValue = 90.0f;
+        obj.ApplyModifiedProperties();
+        NavMeshBuilder.BuildNavMesh();
+    }
+}
